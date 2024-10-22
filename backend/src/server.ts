da24208de8e00b648e7ac6eef228a75f3f49e945
tiagoforward <devtiagoforward@gmail.com>
2024-10-22 13:16:40 -0300

    updating rename tables

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,46 @@
 import fastify from 'fastify';
+import crypto from 'node:crypto'
 import { knex } from './database';
 
 const app = fastify({
   logger: true
 });
 
+// app.get('/products', async function handler(request, reply) {
+//   try {
+//     const products = await knex('products').insert({
+//       id: crypto.randomUUID(),
+//       title: 'Vela de Baunilha',
+//       description: 'Vela aromática de Baunilha 100g',
+//       price: 10,
+//       stock_quantity: 5,
+//     }).returning('*')
+
+//     return reply.send(products);
+//   } catch (error) {
+//     reply.status(500).send({ error: 'Failed to fetch products' });
+//   }
+// });
+
 app.get('/products', async function handler(request, reply) {
   try {
-    const products = await knex('products').select('*');
+    const products = await knex('products').select('*')
 
-    return reply.send(products);
+    return products
   } catch (error) {
     reply.status(500).send({ error: 'Failed to fetch products' });
   }
 });
+
+// app.get('/orders', async function handler(request, reply) {
+//   try {
+//     const orders = await knex('orders').select('*');
+
+//     return reply.send(orders);
+//   } catch (error) {
+//     reply.status(500).send({ error: 'Failed to fetch orders' });
+//   }
+// });
 
 (async () => {
   try {
