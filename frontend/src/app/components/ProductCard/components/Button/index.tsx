8db6df8bd5be8735f60540d0d67aca274b0631ee
tiagoforward <devtiagoforward@gmail.com>
2024-11-19 12:07:45 -0300

    inserted toaster when adding products to cart

--- frontend/src/app/components/ProductCard/components/Button/index.tsx
+++ frontend/src/app/components/ProductCard/components/Button/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useFormStatus } from "react-dom"
+import { useToast } from "@/hooks/use-toast"
 
 interface BuyButtonProps {
     name: string
@@ -8,9 +9,18 @@ interface BuyButtonProps {
 
 export function BuyButton({ name }: BuyButtonProps) {
     const { pending } = useFormStatus()
+    const { toast } = useToast()
 
     return (
-        <button disabled={pending} className="rounded-md text-white bg-neutral-900 hover:opacity-80 px-4 py-1 tracking-wider w-full">
+        <button 
+            disabled={pending} 
+            className="rounded-md text-white bg-neutral-900 hover:opacity-80 px-4 py-1 tracking-wider w-full" 
+            onClick={() => {
+                toast({
+                    title: "Produto adicionado ao carrinho.",
+                    style: { color: "#D81B60", borderColor: "#D81B60"}
+                })
+            }}>
             {pending ? "Adicionando..." : name}
         </button>
     )
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NotificationBanner from "./components/NotificationBanner";
 import { CartProvider } from "@/contexts/CartContext";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </CartProvider>
+        <Toaster />
       </body>
     </html>
   );
